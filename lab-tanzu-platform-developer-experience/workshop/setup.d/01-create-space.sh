c8e64fb02d60520085b0641800f153c0ea25b658
Timo Salm <52704922+timosalm@users.noreply.github.com>
2024-07-11 10:36:49 +0200

    Update 01-create-space.sh

--- lab-tanzu-platform-developer-experience/workshop/setup.d/01-create-space.sh
+++ lab-tanzu-platform-developer-experience/workshop/setup.d/01-create-space.sh
@@ -8,6 +8,8 @@ TANZU_API_TOKEN=$TANZU_CLI_SPACE_CREATE_TOKEN tanzu context create space-admin -
 
 tanzu project use $TANZU_PLATFORM_PROJECT
 
+export TANZU_BUILD_PLAN=$(kubectl get containerappbuildplans simple.tanzu.vmware.com -o json)
+
 tanzu space create $SESSION_NAME $(echo $TANZU_PLATFORM_PROFILES | tr , '\n' | awk '{print "--profile " $1}' | tr '\n' ' ') $(echo $TANZU_PLATFORM_AVAILABILITY_TARGETS | tr , '\n' | awk '{print "--availability-target " $1}' | tr '\n' ' ') --update-strategy RollingUpdate -y --no-color
 
 tanzu context delete space-admin -y
