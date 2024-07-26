d490c5e58ec3db98185126465dae3e2377c353a2
Timo Salm <accounts@timosalm.de>
2024-07-26 15:18:39 +0200

    Improvements

--- lab-platform-engineer/educates/workshop/setup.d/01-vcluster-setup.sh
+++ lab-platform-engineer/educates/workshop/setup.d/01-vcluster-setup.sh
@@ -1,4 +1,8 @@
-curl -L -o vcluster "https://github.com/loft-sh/vcluster/releases/download/v0.20.0-beta.9/vcluster-linux-amd64" && chmod +x vcluster && mv vcluster $HOME/bin/
+#!/bin/bash
+
+set -x
+set -eo pipefail
+
 cat <<EOF >> vcluster.yaml
 controlPlane:
   proxy:
--- lab-platform-engineer/educates/workshop/setup.d/03-terminal-setup.sh
+++ lab-platform-engineer/educates/workshop/setup.d/03-terminal-setup.sh
@@ -3,22 +3,6 @@
 set -x
 set -eo pipefail
 
-(
-  set -x; cd "$(mktemp -d)" &&
-  OS="$(uname | tr '[:upper:]' '[:lower:]')" &&
-  ARCH="$(uname -m | sed -e 's/x86_64/amd64/' -e 's/\(arm\)\(64\)\?.*/\1\2/' -e 's/aarch64$/arm64/')" &&
-  KREW="krew-${OS}_${ARCH}" &&
-  curl -fsSLO "https://github.com/kubernetes-sigs/krew/releases/latest/download/${KREW}.tar.gz" &&
-  tar zxvf "${KREW}.tar.gz" &&
-  ./"${KREW}" install krew
-)
-export PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"
-
-kubectl krew install ctx
-kubectl krew install ns
-kubectl krew install konfig
-kubectl krew install eksporter
-
 kubectl konfig import --save vcluster-kubeconfig.yaml
 kubectl ctx $(yq eval '.current-context' vcluster-kubeconfig.yaml)
 kubectl ns default
